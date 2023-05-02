1f68f6c25a74779a571aa8a6014017f374bc9355
Alexander Evgin <aleasims@gmail.com>
2023-05-02 18:57:47 +0000

    Initial implementation of Pallas base field type

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,88 @@
 //! zkLLVM type wrappers.
+
+#![feature(restricted_std)]
+
+use std::fmt;
+use std::ops::{Add, Sub, Neg, Div, Mul, AddAssign, SubAssign, MulAssign, DivAssign};
+
+/// Pallas base field type.
+///
+/// Wrapper for `__zkllvm_field_pallas_base` type.
+#[derive(Clone, Copy, Default, Eq, PartialEq)]
+pub struct PallasBase(pub __zkllvm_field_pallas_base);
+
+impl Add for PallasBase {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self(self.0.add(other.0))
+    }
+}
+
+impl AddAssign for PallasBase {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0.add_assign(rhs.0);
+    }
+}
+
+impl fmt::Debug for PallasBase {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl fmt::Display for PallasBase {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl Div for PallasBase {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self::Output {
+        Self(self.0.div(other.0))
+    }
+}
+
+impl DivAssign for PallasBase {
+    fn div_assign(&mut self, other: Self) {
+        self.0.div_assign(other.0);
+    }
+}
+
+impl Mul for PallasBase {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self(self.0.mul(other.0))
+    }
+}
+
+impl MulAssign for PallasBase {
+    fn mul_assign(&mut self, other: Self) {
+        self.0.mul_assign(other.0);
+    }
+}
+
+impl Neg for PallasBase {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(self.0.neg())
+    }
+}
+
+impl Sub for PallasBase {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0.sub(other.0))
+    }
+}
+
+impl SubAssign for PallasBase {
+    fn sub_assign(&mut self, other: Self) {
+        self.0.sub_assign(other.0);
+    }
+}
